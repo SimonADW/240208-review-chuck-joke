57c4412d5a3bfa0780c7799d42c55a8d4e1c9a66
Simon A.D. Winter <112318684+SimonADW@users.noreply.github.com>
2024-02-08 10:42:13 +0100

    Declare fetchJoke function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-console.log("Test");
+// GET DOM ELEMENTS
+const jokeButton = document.querySelector(".get-joke-button");
+const jokeContainer = document.querySelector(".joke-container");
+
+// FETCH JOKE FUNCTION
+
+const fetchJoke = async ()=> {
+	const response = await fetch("https://api.chucknorris.io/jokes/random");
+	const data = await response.json();
+	renderJoke(data.value)
+}
+
+fetchJoke()
